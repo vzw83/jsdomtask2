318ba156f379d071a36a8c32f3868ceed3f3bea1
vzw83 <vzw83@yandex.ru>
2023-12-09 19:36:29 +0300

    dragFinish

--- app.js
+++ app.js
@@ -11,6 +11,7 @@ const createRow = (str) => {
 };
 
 const createAction = (event) => {
+  // console.log(event)
   const type = event.type;
   CR.innerText = type;
   RL.prepend(createRow(type));
@@ -27,31 +28,45 @@ EXPM.addEventListener("mouseleave", createAction);
 EXPM.addEventListener("focus", createAction);
 EXPM.addEventListener("blur", createAction);
 
-const keyDown = (event) =>{
-    createAction(event)
-    if(event.keyCode === 37){
-        EXPK.style.left = `${+EXPK.style.left.split('px')[0] - 5}px`
-    }
-    else if(event.keyCode === 39){
-        EXPK.style.left = `${+EXPK.style.left.split('px')[0] + 5}px`
-    }
-    else if(event.keyCode === 38){
-        EXPK.style.top = `${+EXPK.style.top.split('px')[0] - 5}px`
-    }
-    else if(event.keyCode === 40){
-        EXPK.style.top = `${+EXPK.style.top.split('px')[0] + 5}px`
-    }   
-}
-
+const keyDown = (event) => {
+  // console.log(event)
+  createAction(event);
+  if (event.keyCode === 37) {
+    EXPK.style.left = `${+EXPK.style.left.split("px")[0] - 5}px`;
+  } else if (event.keyCode === 39) {
+    EXPK.style.left = `${+EXPK.style.left.split("px")[0] + 5}px`;
+  } else if (event.keyCode === 38) {
+    EXPK.style.top = `${+EXPK.style.top.split("px")[0] - 5}px`;
+  } else if (event.keyCode === 40) {
+    EXPK.style.top = `${+EXPK.style.top.split("px")[0] + 5}px`;
+  }
+};
 
 EXPK.addEventListener("keydown", keyDown);
 EXPK.addEventListener("keypress", createAction);
 EXPK.addEventListener("keyup", createAction);
 
+let dragX = 0
+let dragY = 0
 
 const drag = (event) => {
-    
-}
+  // console.log(event)
+  createAction(event);
+
+  if (event.type === "dragstart") {
+    EXPD.classList.add('dragged')
+    // x
+    dragX = event.clientX;
+    // y
+    dragY = event.clientY;
+  } else if (event.type === "dragend") {
+    EXPD.classList.remove('dragged')
+    // x
+    EXPD.style.left = `${+EXPD.style.left.split("px")[0] + (event.clientX - dragX)}px`;
+    // y
+    EXPD.style.top = `${+EXPD.style.top.split("px")[0] + (event.clientY - dragY)}px`;
+  }
+};
 
 EXPD.addEventListener("dragstart", drag);
 EXPD.addEventListener("dragend", drag);
